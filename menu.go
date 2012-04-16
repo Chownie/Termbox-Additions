50dda227b336228aa9aa0d334d993d940dd88bae
chownplusx <sunderphon@gmail.com>
2012-04-16 23:09:32 +0000

    GetMenuDimensions renamed to GetMenuSize

--- menu.go
+++ menu.go
@@ -5,7 +5,7 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-func GetMenuDimensions(text string, options []string) (int, int) {
+func GetMenuSize(text string, options []string) (int, int) {
 	width := len(text)
 	for i := 0; i < len(options); i++ {
 		if len(options[i]) > width {
@@ -30,7 +30,7 @@ func DrawSelection(x, y int, sel int) {
 
 func DrawMenu(x, y int, text string, options []string) int {
 	selection := 0
-	width, height := GetMenuDimensions(text, options)
+	width, height := GetMenuSize(text, options)
 
 loop:
 	for {
