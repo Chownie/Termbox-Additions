8f3dc7d28c559f633ca15b90bfc8b553b6381af1
asa dawson <asa@lulu>
2014-03-30 04:26:46 +0100

    Added options to drawing the menu

--- menu.go
+++ menu.go
@@ -5,6 +5,12 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+const (
+	AL_LEFT = iota
+	AL_RIGHT
+	AL_CENTER
+)
+
 func GetMenuSize(text string, options []string) (int, int) {
 	width := len(text)
 	for i := 0; i < len(options); i++ {
@@ -28,15 +34,27 @@ func DrawSelection(x, y int, sel int) {
 	DrawText(x+1, y+1+sel, ">")
 }
 
-func DrawMenu(x, y int, text string, options []string) int {
+func DrawMenu(x, y int, text string, options []string, option int) int {
 	selection := 0
 	width, height := GetMenuSize(text, options)
 
 loop:
 	for {
+		if option == AL_LEFT {
+			x = 0
+		} else if option == AL_CENTER {
+			x = (x/2) - (width/2)
+			y = (y/2) - (height/2)
+		} else if option == AL_RIGHT {
+			x = x-width
+			y = y-height
+		}
+		
 		DrawBox(x, y, width, height)     // Draw the surrounding box
 		DrawSelection(x, y, selection)   // Draw the selector arrow
 		DrawOptions(x, y, options, text) // Draw the option text + title
+		
+
 		termbox.Flush()
 
 		switch ev := termbox.PollEvent(); ev.Type {
